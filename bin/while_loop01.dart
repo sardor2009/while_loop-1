587f8b7738ad7c43838e57d7aaa1383ec6732648
sardor <ruziqulovsardor3@gmail.com>
2022-08-11 14:29:43 +0500

    code

--- bin/while_loop01.dart
+++ bin/while_loop01.dart
@@ -2,4 +2,19 @@
   Create function func with "a " argument
   Return the number of odd numbers up to A.
 */
-void main() {}
+int func(int a) {
+   int i = 0;
+   int s = 0;
+   while (i < a) {
+    i += 1;
+    if (i % 2 == 1){
+      s += 1;
+    }
+   }
+  return s;
+  }
+
+
+void main() {
+  print(func(12));
+}
